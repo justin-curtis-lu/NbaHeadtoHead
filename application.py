a5c212d4c7a3d2b4a007fbb4183dc87e44432dd8
justin-curtis-lu <67398801+justin-curtis-lu@users.noreply.github.com>
2021-01-12 01:03:13 -0800

    Update application.py

--- application.py
+++ application.py
@@ -7,16 +7,11 @@ def mainNba():
 header_text = '''
     <html>\n<head> <title>NBA HeadtoHead</title> </head>\n<body>'''
 
-# EB looks for an 'application' callable by default.
 application = Flask(__name__)
 
-# add a rule for the index page.
 application.add_url_rule('/', 'index', (lambda: header_text +
     mainNba()))
 
-# run the app.
 if __name__ == "__main__":
-    # Setting debug to True enables debug output. This line should be
-    # removed before deploying a production app.
     application.debug = True
     application.run()
